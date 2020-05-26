b5c64609d015949acbf767bb09a913233cbb02f2
ssun912 <sallysun.912@gmail.com>
2020-05-26 17:39:35 -0400

    Updates

---
+++ precache-manifest.b5bb789652063c2ee011fa6c54348e14.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76ca7518dd3dad453115a01fa7c1e83",
+    "url": "/lanternfestival/index.html"
+  },
+  {
+    "revision": "aa7f2530e4a107b5a182",
+    "url": "/lanternfestival/static/css/main.d25a618a.chunk.css"
+  },
+  {
+    "revision": "75f2f2b3aa5463ef5e23",
+    "url": "/lanternfestival/static/js/2.08250c12.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/lanternfestival/static/js/2.08250c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7f2530e4a107b5a182",
+    "url": "/lanternfestival/static/js/main.77150a72.chunk.js"
+  },
+  {
+    "revision": "9b2f2eda1f6f93457e8d",
+    "url": "/lanternfestival/static/js/runtime-main.ae18c79b.js"
+  },
+  {
+    "revision": "ebede0bf43e2dcf0e927085f70ab6c16",
+    "url": "/lanternfestival/static/media/favicon.ebede0bf.png"
+  },
+  {
+    "revision": "37b1d44482bc1bfda0e457ac586012f9",
+    "url": "/lanternfestival/static/media/slide1.37b1d444.JPG"
+  },
+  {
+    "revision": "e5e1fde5bc3aa27a0ee1f246846e13fa",
+    "url": "/lanternfestival/static/media/slide2.e5e1fde5.JPG"
+  },
+  {
+    "revision": "5fffab94994e3b72685aea8b8d0d3ff4",
+    "url": "/lanternfestival/static/media/slide3.5fffab94.JPG"
+  },
+  {
+    "revision": "9395443fa7ac34b123c9f4bb2aad99be",
+    "url": "/lanternfestival/static/media/slide4.9395443f.JPG"
+  },
+  {
+    "revision": "dbf3c0e7c779e5f551c66c6c3f9aab7d",
+    "url": "/lanternfestival/static/media/slide5.dbf3c0e7.JPG"
+  },
+  {
+    "revision": "4485509aa86f403bdbcf3983c056e066",
+    "url": "/lanternfestival/static/media/slide6.4485509a.JPG"
+  },
+  {
+    "revision": "9940af97841fba7d0ff7fe8f1c5e806a",
+    "url": "/lanternfestival/static/media/slide7.9940af97.JPG"
+  }
+]);
