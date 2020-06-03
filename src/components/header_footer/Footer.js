adccaa697cfcfe605acd9fe2b246a98e39f140cd
ssun912 <sallysun.912@gmail.com>
2020-06-03 11:54:29 -0400

    ''

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -6,7 +6,10 @@ const Footer = () => {
         <footer>
             <Fade delay={500}>
                 <div className="font_righteous footer_logo_venue">The Lantern Festival</div>
-                <div className="footer_copyright">NYC Lantern Festival @{(new Date(Date.now())).getFullYear()}. All Rights Reserved.</div>
+                <div className="footer_copyright">
+                    NYC Lantern Festival @{(new Date(Date.now())).getFullYear()}. All Rights Reserved.
+                    <p>Photo by Lucas Li | Web power by Sally Sun</p>
+                </div>
             </Fade>
         </footer>
     );
