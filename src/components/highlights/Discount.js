4da61d990b68495abdcbe5c22605f77c55cb0296
ssun912 <sallysun.912@gmail.com>
2020-07-22 12:01:25 -0400

    'update'

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,67 +1,65 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 import MyButton from '../utils/MyButton';
 
-class Discount extends Component {
-    
-    state = {
-        discountStart: 0,
-        discountEnd: 30,
-    }
-    
-    computeDate() {
-        const date = new Date(this.props.date);
+const Discount = (props) => {
+    const totalDiscount = 30;
+    const [ startCounting, setStartCounting ] = useState(false);
+    const [ discount, setDiscount ] = useState(0);
+
+    const computeDate = () => {
+        const date = new Date(props.date);
         const discount_date = new Date(date.getFullYear(), date.getMonth(), date.getDate()-30);
         return discount_date.toDateString();
     }
 
-    upPercentage = () => {
-        if(this.state.discountStart < this.state.discountEnd) {
-            this.setState({ 
-                discountStart: this.state.discountStart +1,
-            })
+    let timeOut;
+    useEffect(() => {
+        if(startCounting) {
+            timeOut = setTimeout(() => {upPercentage()}, 50);
+            if(discount >= totalDiscount) setStartCounting(false);
+        }
+        return (() => clearTimeout(timeOut));
+    })
+
+    const upPercentage = () => {
+        if(discount < totalDiscount) {
+            setDiscount(discount+1);
         }
     }
 
-    componentDidUpdate() {
-        setTimeout(()=> {
-            this.upPercentage();
-        }, 30);
-    }
-
-    render() {
-        return (
-            <div className="center_wrapper">
-                <div className="discount_wrapper">
-                    
-                    <Fade
-                        onReveal = {()=> this.upPercentage()}
-                    >
-                    <div className="discount_percentage">
-                        <span>{this.state.discountStart}%</span>
-                        <span>OFF</span>
-                    </div>
-                    </Fade>
-                    
-                    <Slide right>
-                    <div className="discount_description">
-                        <p>Purchase tickets before {this.computeDate()}</p>
-                        <p>Or bulk purchase with a group of 15+ people!</p>
-                        
-                        <MyButton 
-                            text="Buy Now!"
-                            backgroundColor="orange"
-                            icon="confirmation_number"
-                            link="/"
-                        />
-                    </div>
-                    </Slide>
-
+    return (
+        <div className="center_wrapper">
+            <div className="discount_wrapper">
+                
+                <Fade onReveal = {
+                    () => { setStartCounting(true); }
+                }>
+                <div className="discount_percentage">
+                    <span>{discount}%</span>
+                    <span>OFF</span>
                 </div>
+                </Fade>
+                
+                <Slide right>
+                <div className="discount_description">
+                    <p>Purchase tickets before {computeDate()}</p>
+                    <p>Or bulk purchase with a group of 15+ people!</p>
+                    
+                    <MyButton 
+                        text="Buy Now!"
+                        backgroundColor="orange"
+                        icon="confirmation_number"
+                        link="/"
+                    />
+                </div>
+                </Slide>
+
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Discount;
+
